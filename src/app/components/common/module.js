0c057de66653edad86754f303b5e69aa224903ca
chunzujun <464249963@qq.com>
2015-10-01 03:28:47 +0800

    add module

--- src/app/components/common/module.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by ChunZuJun on 2015/9/30.
- *
- */
-angular
-  .module('bb.cp.common', []); //cp: components
---
+++ src/app/components/personalcenter/controller.js
@@ -0,0 +1,13 @@
+/**
+ * Created by chunzj on 2015/10/1.
+ */
+(function () {
+  angular
+    .module('bb')
+    .controller('PersonalCenterController', PersonalCenterController);
+
+  /** @ngInject */
+  function PersonalCenterController() {
+
+  }
+})();
