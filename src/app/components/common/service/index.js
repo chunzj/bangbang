951829d7e86b4a69e81790e813eef359ff95147e
chunzujun <464249963@qq.com>
2015-11-03 23:36:34 +0800

    modify base

--- src/app/components/common/service/index.js
+++ src/app/components/common/service/index.js
@@ -5,91 +5,116 @@
 
   angular
     .module('bb.common.service', ['bb.common.config', 'bb.common.constant'])
-    .service('ajaxRequest', function($rootScope, $http, $log, bbConfig) {
+    .service('ajaxRequest', function ($rootScope, $http, $log, bbConfig) {
 
-        var apiConfig = bbConfig.apiConfig[$rootScope.env || 'dev'];
+      var apiConfig = bbConfig.apiConfig[$rootScope.env || 'dev'];
 
-        function _ajaxRequest (options, apiPath) {
+      function _ajaxRequest(options, apiPath) {
 
-          if (!bbConfig.apiPath[apiPath]) {
-            throw new Error('找不到您要访问的api,请检查模块bb.common.config中的apiPath是否配置。');
-          }
-
-          var defaultOptions = {
-            url: getApiUrl(bbConfig.apiPath[apiPath]),
-            responseType: 'json',
-            timeout: 2000,
-            transformResponse: function (data, headersGetter) {
-              $log.info(data);
-              $log.info(headersGetter);
-            }
-          };
-
-          angular.extend(options, defaultOptions);
-
-          return $http(options);
+        if (!bbConfig.apiPath[apiPath]) {
+          throw new Error('找不到您要访问的api,请检查模块bb.common.config中的apiPath是否配置。');
         }
 
-        function ajaxPost (formData, apiPath) {
-          return _ajaxRequest({
-            method: 'POST',
-            data: formData,
-            headers: {
-              'Content-Type': 'application/json;charset=utf-8'
-            }
-          }, apiPath);
-        }
-
-        /**
-         *
-         * @param formData {Object} 除文件对象以外的其他表单数据，格式为json
-         * @param fileKey {String}
-         * @param fileObject 文件对象, @seeTo https://developer.mozilla.org/en-US/docs/Web/API/File
-         * @param apiPath {String}
-         * @returns {*}
-         */
-        function ajaxFileUpload (formData, fileKey, fileObject, apiPath) {
-
-          if (!fileObject || !(fileObject instanceof File)) {
-            throw new TypeError('无效的文件对象');
-          }
-
-          var _formData = new FormData();
-          if (formData) {
-            for (var key in formData) {
-              _formData.append(key, formData[key]);
-            }
-          }
-          _formData.append(fileKey, fileObject);
-
-          return _ajaxRequest({
-            method: 'POST',
-            data: _formData,
-            headers: {
-              'Content-Type': undefined
-            }
-          }, apiPath);
-        }
-
-        function ajaxGet (queryData, apiPath) {
-          return _ajaxRequest({
-            method: 'GET',
-            params: queryData,
-            headers: {
-              'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
-            }
-          }, apiPath);
-        }
-
-        return {
-          get: ajaxGet,
-          post: ajaxPost,
-          fileUpload: ajaxFileUpload
+        var defaultOptions = {
+          url: getApiUrl(bbConfig.apiPath[apiPath]),
+          responseType: 'json',
+          timeout: 2000
         };
 
-        function getApiUrl (apiPath) {
-          return ['http://', apiConfig.hostname, ':', apiConfig.port, bbConfig.apiBase, apiPath].join('');
+        angular.extend(options, defaultOptions);
+
+        return $http(options);
+      }
+
+      function ajaxPost(formData, apiPath) {
+        return _ajaxRequest({
+          method: 'POST',
+          data: formData,
+          headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+          }
+        }, apiPath);
+      }
+
+      /**
+       *
+       * @param formData {Object} 除文件对象以外的其他表单数据，格式为json
+       * @param fileKey {String}
+       * @param fileObject 文件对象, @seeTo https://developer.mozilla.org/en-US/docs/Web/API/File
+       * @param apiPath {String}
+       * @returns {*}
+       */
+      function ajaxFileUpload(formData, fileKey, fileObject, apiPath) {
+
+        if (!fileObject || !(fileObject instanceof File)) {
+          throw new TypeError('无效的文件对象');
         }
+
+        var _formData = new FormData();
+        if (formData) {
+          for (var key in formData) {
+            _formData.append(key, formData[key]);
+          }
+        }
+        _formData.append(fileKey, fileObject);
+
+        return _ajaxRequest({
+          method: 'POST',
+          data: _formData,
+          headers: {
+            'Content-Type': undefined
+          }
+        }, apiPath);
+      }
+
+      function ajaxGet(queryData, apiPath) {
+        return _ajaxRequest({
+          method: 'GET',
+          params: queryData,
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
+          }
+        }, apiPath);
+      }
+
+      function responseInteceptor(processFn) {
+        return function () {
+          var args = arguments;
+          return $q(function (resolve, reject) {
+            processFn.apply(null, Array.prototype.slice.call(args)).then(function (result) {
+              var realData = result.data;
+              if (realData.code == 1) {
+                resolve(realData.data);
+              } else if (realData.code == 0) {
+                reject({
+                  code: realData.msgCode,
+                  msg: realData.msg
+                });
+              } else {
+                $log.info(realData);
+                reject({
+                  msg: '网络异常，请稍候重试！'
+                });
+              }
+            }).catch(function (error) {
+              $log.info(error);
+              reject({
+                msg: '网络异常，请稍候重试！'
+              });
+            });
+          });
+        }
+      }
+
+      return {
+        get: responseInteceptor(ajaxGet),
+        post: responseInteceptor(ajaxPost),
+        fileUpload: responseInteceptor(ajaxFileUpload)
+      };
+
+      function getApiUrl(apiPath) {
+        return ['http://', apiConfig.hostname, ':', apiConfig.port, bbConfig.apiBase, apiPath].join('');
+      }
     })
     .service('checker', function ($q, $window, $log, ajaxRequest, bbConstant) {
       return {
@@ -132,19 +157,25 @@
           });
         }
       };
-    }).service('apiService', function ($q, $log, ajaxRequest, bbConfig) {
-        return {
-          getBaseData: function () {
-            return $q(function (resolve, reject) {
-              ajaxRequest.get({}, 'baseData').then(function (result) {
-                $log.info('@@@@@@@@@@@@@@');
-                $log.info(result);
-              }).catch(function (error) {
-                $log.info('##############');
-                $log.info(error);
-              });
+    }).service('apiService', function ($window, $q, $log, ajaxRequest, bbConfig) {
+      return {
+        getBaseData: function () {
+          return $q(function (resolve, reject) {
+
+            if ($window.baseData) {
+              resolve();
+              return;
+            }
+
+            ajaxRequest.get({}, 'baseData').then(function (result) {
+              $window.baseData = result;
+              resolve();
+            }).catch(function (error) {
+              $log.info('Fail to get base data ' + error);
+              reject();
             });
-          }
-        };
-      });
+          });
+        }
+      };
+    });
 })();
